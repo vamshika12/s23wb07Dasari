a8d34901c80d994ae2b9bf50366bef9db6b0c435
vamshika12 <vamshikadasari15@gmail.com>
2023-04-05 19:40:11 -0500

    files are modified

--- routes/Dog.js
+++ routes/Dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Dog', { title: 'Search Results Dog' });
+  res.render('dog', { title: 'Search Results Dog' });
 });
 
 module.exports = router;
